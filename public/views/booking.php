1985eb0c7cb5f4aea60c7bc9e40b18fb992b7d3b
James-gif-ux <jjbright0402@gmail.com>
2025-01-27 15:44:58 +0800

    18th commit

--- public/views/booking.php
+++ public/views/booking.php
@@ -6,7 +6,7 @@
   $connector = new Connector();
 
   // Fetch all bookings that are pending approval
-  $sql = "SELECT booking_id, booking_name, booking_email, booking_number, booking_date, booking_time, booking_status FROM booking_tb WHERE booking_status IN ('pending', 'approved','completed')";
+  $sql = "SELECT booking_id, booking_fullname, booking_email, booking_number, booking_date, booking_time, booking_status FROM booking_tb WHERE booking_status IN ('pending', 'approved','completed')";
 
   $bookings = $connector->executeQuery($sql);
 ?>
