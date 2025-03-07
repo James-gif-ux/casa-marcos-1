18adea6f6c8fb2659ee0944b8754a4c4072cb313
James-gif-ux <jjbright0402@gmail.com>
2025-03-07 14:11:35 +0800

    85 commit

--- public/views/food.php
+++ public/views/food.php
@@ -1,3 +1,26 @@
 <?php
     include 'nav/header.php';
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div>
+        <h1>FOOD MENU</h1>
+        <div>
+            <input type="text" placeholder="FOOD MENU" id="foodMenu" required> 
+            <input type="text" placeholder="PRICE" id="price" required>
+        </div>
+        <div>
+            <textarea name="food" id="" required></textarea>
+        </div>
+        <div>
+            <button type="submit">Upload Photo</button>
+        </div>
+    </div>
+</body>
+</html>
